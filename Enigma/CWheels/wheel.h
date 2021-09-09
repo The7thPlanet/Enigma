4fff20cfe7d007b6ba1d1663ad1dc2ea8ff3a671
Hilal-Almoqbali <hilalalmoqbali2060@gmail.com>
2021-09-09 21:06:45 +0400

    there is some things fixed and better code

--- Enigma/CWheels/wheel.h
+++ Enigma/CWheels/wheel.h
@@ -2,10 +2,15 @@
 
 #include <string.h>
 #include <stdio.h>
+#define __STDC_WANT_DEC_FP__
 #include <stdlib.h>
-#include <fun/LTN.h>
 
-char *main(char *messageI[] , char *key )
+
+
+char *wheel(char *messageI , char *key )
 {
-
+    //_Decimal64 d64;
+    char *DMessage;
+    strtod(messageI, &DMessage);
+    return DMessage;
 }
